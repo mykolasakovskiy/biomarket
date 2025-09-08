34aa7ca9fa03c1e08a2a5d8c372edad8428e698f
mykolasakovskiy <150294348+mykolasakovskiy@users.noreply.github.com>
2025-09-08 22:26:47 +0300

    Update urls.py

--- biomarket/urls.py
+++ biomarket/urls.py
@@ -4,10 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('shop.urls', namespace='shop')),
+    path("admin/", admin.site.urls),
+
+    # Підключаємо shop з namespace='shop'
+    path("", include(("shop.urls", "shop"), namespace="shop")),
 ]
 
+# У продакшені статику віддає WhiteNoise; тут додаємо лише медіа під час DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
